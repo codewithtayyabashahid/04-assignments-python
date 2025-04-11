dbc6a98767fbb0df70eed65fcd23778e6aafa87c
codewithtayyabashahid <167111800+codewithtayyabashahid@users.noreply.github.com>
2025-04-11 21:49:39 +0500

    Add files via upload

---
+++ homework_projects/00_intro_python/02_agreement_bot.py
@@ -0,0 +1,11 @@
+def main():
+    # Ask the user for their favorite animal
+    favorite_animal = input("What's your favorite animal? ")
+    
+    # Print the response with the user's input
+    print(f"My favorite animal is also {favorite_animal}!")
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
---
+++ homework_projects/00_intro_python/06_square_number.py
@@ -0,0 +1,12 @@
+def main():
+    # Ask the user for a number
+    num = float(input("Type a number to see its square: "))
+    
+    # Calculate the square
+    squared = num * num
+    
+    # Print the result
+    print(f"{num} squared is {squared}")
+
+if __name__ == '__main__':
+    main()
